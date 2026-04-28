85dd2d7a05b90af8f42515ebb7d40d1298d1333b
affsvoyo <githubunoficial@gmail.com>
2026-04-28 14:15:18 -0300

    Refactor CustomWaveShader to use external fragment shader

--- source/states/CustomWaveShader.hx
+++ source/states/CustomWaveShader.hx
@@ -2,46 +2,44 @@ package states;
 
 import flixel.system.FlxAssets.FlxShader;
 
+#if sys
+import sys.io.File;
+#end
+
 class CustomWaveShader extends FlxShader
 {
+    #if sys
+    @:glFragmentSource(File.getContent("assets/data/shader/Wavy.frag"))
+    #else
     @:glFragmentSource('
         #pragma header
-		//uniform float tx, ty; // x,y waves phase
-		uniform float uTime;
-		
-		/**
-		 * How fast the waves move over time
-		 */
-		uniform float uSpeed;
-		
-		/**
-		 * Number of waves over time
-		 */
-		uniform float uFrequency;
-		
-		/**
-		 * How much the pixels are going to stretch over the waves
-		 */
-		uniform float uWaveAmplitude;
 
-		vec2 sineWave(vec2 pt)
-		{
-			float x = 0.0;
-			float y = 0.0;
-			
-				float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;
-                float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;
-				pt.y += offsetY;
-				pt.x += offsetX;
-			return vec2(pt.x + x, pt.y + y);
-		}
+        uniform float uTime;
+        uniform float uSpeed;
+        uniform float uFrequency;
+        uniform float uWaveAmplitude;
 
-		void main()
-		{
-			vec2 uv = sineWave(openfl_TextureCoordv);
-			gl_FragColor = texture2D(bitmap, uv);
-}
+        vec2 sineWave(vec2 pt)
+        {
+            float x = 0.0;
+            float y = 0.0;
+
+            float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;
+            float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;
+
+            pt.y += offsetY;
+            pt.x += offsetX;
+
+            return vec2(pt.x + x, pt.y + y);
+        }
+
+        void main()
+        {
+            vec2 uv = sineWave(openfl_TextureCoordv);
+            gl_FragColor = texture2D(bitmap, uv);
+        }
     ')
+    #end
 
     public function new()
     {
