d4f639a86d5d1f5253d85a4228372c8dbec7404a
affsvoyo <githubunoficial@gmail.com>
2026-04-28 07:35:35 -0300

    Implement dynamic state selection in Main class
    Add configuration state handling based on boot file.

--- source/Main.hx
+++ source/Main.hx
@@ -3,10 +3,29 @@ package;
 import openfl.display.Sprite;
 import flixel.FlxGame;
 import states.PlayState;
+import states.ConfigState;
+import sys.FileSystem;
+import sys.io.File;
 
-class Main extends Sprite {
-    public function new() {
+class Main extends Sprite
+{
+    public function new()
+    {
         super();
-        addChild(new FlxGame(1280, 720, PlayState));
+
+        var firstState:Class<Dynamic>;
+        var bootPath = "assets/data/firstboot.txt";
+
+        if (!FileSystem.exists(bootPath))
+        {
+            firstState = ConfigState;
+        }
+        else
+        {
+            var content = File.getContent(bootPath);
+            firstState = (content.indexOf("configured=true") != -1) ? PlayState : ConfigState;
+        }
+
+        addChild(new FlxGame(1280, 720, firstState));
     }
 }
